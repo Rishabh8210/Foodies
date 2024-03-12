2e6563593c2ed6bb2a5f66a5f7f5cdb71e01cc8e
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-03-12 11:08:38 +0530

    Footer done

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import Header from "./src/Components/Header.js"
 import Body from "./src/Components/Body.js"
+import Footer from "./src/Components/Footer.js"
 
 const AppLayout = () => {
     return (
         <>
             <Header />
             <Body />
+            <Footer />
         </>
     )
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+const Footer = () => {
+    return (
+        <h1>I am a Foooter</h1>
+    )
+}
+export default Footer;
